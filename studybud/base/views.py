bf843a525ab77b4c1d0238e5b2371dbabff09291
Amedi13 <160151075+Amedi13@users.noreply.github.com>
2025-02-23 23:18:54 -0500

    static files

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -59,8 +59,10 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.all().order('-created').filer(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count
+              ,'room_messages': room_messages }
     return render(request, 'base/home.html', context)
 
 def room(request, pk): 
@@ -79,13 +81,24 @@ def room(request, pk):
     context = {'room': room, 'messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
+def userProfile(request, pk): 
+    user = User.onjects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages,
+               'topics': topics} 
+    return render(request, 'base/profile.html', context)
+
 @login_required(login_url='/login')
 def createRoom(request): 
     form = RoomForm()
     if request.method == 'POST': 
         form = RoomForm(request.POST)
         if form.is_valid(): 
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context = {'form': form}
